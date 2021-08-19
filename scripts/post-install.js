b17b5b24c0ef8d7acb979275b072b519e5666cb2
xinwenchao <boat3471@me.com>
2021-08-19 17:35:42 +0800

    修正 eslint-plugin-vuetify 改造逻辑问题；

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -17,6 +17,6 @@ if (!fs.existsSync(resolve(devTargetFile))) {
   const gridUnknownAttributesPath = path.resolve(__dirname, '..', 'node_modules/eslint-plugin-vuetify/lib/rules/grid-unknown-attributes.js')
   if (fs.existsSync(gridUnknownAttributesPath)) {
     const content = fs.readFileSync(gridUnknownAttributesPath).toString()
-    fs.writeFileSync(gridUnknownAttributesPath, content.replace('vuetify/es5/components', '@zwd/z-ui/es5/components'))
+    fs.writeFileSync(gridUnknownAttributesPath, content.replace(/vuetify\/es5\/components/g, '@zwd/z-ui/es5/components'))
   }
 })()
