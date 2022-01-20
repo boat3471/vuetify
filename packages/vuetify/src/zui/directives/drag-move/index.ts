fcee567065cd034315d162b401e0d57360b4cb31
jichun <jichun@zpmc.com>
2022-01-20 14:09:23 +0800

    解决VDragMove指令存在 拖曳时鼠标移动和元素真实移动偏差的bug

--- packages/vuetify/src/zui/directives/drag-move/index.ts
+++ packages/vuetify/src/zui/directives/drag-move/index.ts
@@ -10,91 +10,106 @@ import { DirectiveOptions } from 'vue'
  *      a. <component v-drag-move /> 整个容器都可触发拖动
  *      b. <component v-drag-move:activitor="['#id1','#id2','.className']" 只有容器内#id1，#id2，.className 的元素可触发拖动
  */
+ function getActivitorEls(el: HTMLElement, binding: any) {
+  if (binding.arg !== 'activitor') return [el];
+  const bindingValue = binding.value && Array.isArray(binding.value) ? binding.value : [];
+  let activitorEls: HTMLElement[] = [];
 
-function getActivitorEls (el: HTMLElement, binding: any) {
-  if (binding.arg !== 'activitor') return [el]
-  const bindingValue = binding.value && Array.isArray(binding.value) ? binding.value : []
-  let activitorEls: HTMLElement[] = []
   for (let i = 0, len = bindingValue.length; i < len; i++) {
-    const selector = bindingValue[i]
-    const activeEls = el.querySelectorAll(selector)
-    activeEls.length && (activitorEls = [...activitorEls, ...activeEls])
+      const selector = bindingValue[i];
+      const activeEls = el.querySelectorAll(selector);
+      activeEls.length && (activitorEls = [...activitorEls, ...activeEls]);
   }
-  return activitorEls.length ? activitorEls : [el]
+
+  return activitorEls.length ? activitorEls : [el];
 }
 
 export const DragMove: DirectiveOptions = {
-  inserted (el: HTMLElement, binding: any) {
-    const dialogEl = el
-    if (!dialogEl) return
-    let isInited = false
-    const activitorEls = getActivitorEls(el, binding)
-    let dialogHeight = 0
-    let dialogWidth = 0
-    let dialogTop = 0
-    let dialogLeft = 0
-    let movedDialogTop = 0
-    let movedDialogLeft = 0
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  inserted(el: HTMLElement, binding: any) {
+      const dialogEl = el;
+      if (!dialogEl) return;
+      let isInited = false;
+      let mouseStartX = 0;
+      let mouseStartY = 0;
+      const activitorEls = getActivitorEls(el, binding);
+      let dialogHeight = 0;
+      let dialogWidth = 0;
+      let dialogY = 0;
+      let dialogX = 0;
+      let newDialogX = 0;
+      let newDialogY = 0;
+      activitorEls.forEach(activitorEl => {
+          // 第一次被拖动时初始化数据
+          activitorEl.addEventListener('mousedown', init);
+          activitorEl.addEventListener('mousedown', addMousemoveEvent);
+      }); // 初始化
 
-    activitorEls.forEach(activitorEl => {
-      // 第一次被拖动时初始化数据
-      activitorEl.addEventListener('mousedown', init)
-      activitorEl.addEventListener('mousedown', addMousemoveEvent)
-    })
+      function init() {
+          if (isInited) {
+              activitorEls.forEach(activitorEl => {
+                  activitorEl.removeEventListener('mousedown', init);
+              });
+              return;
+          }
+          const boundingClientRect = dialogEl.getBoundingClientRect();
+          dialogHeight = dialogEl.offsetHeight;
+          dialogWidth = dialogEl.offsetWidth;
+          dialogY = boundingClientRect.top;
+          dialogX = boundingClientRect.left;
+          dialogEl.style.cssText += `position:fixed; top: ${dialogY}px; left: ${dialogX}px; width:${dialogWidth}px; height:${dialogHeight}px;`;
+          isInited = true;
+      } // 添加鼠标事件
 
-    // 初始化
-    function init () {
-      if (isInited) {
-        activitorEls.forEach(activitorEl => {
-          activitorEl.removeEventListener('mousedown', init)
-        })
-        return
+      function addMousemoveEvent(e: MouseEvent) {
+          e.preventDefault();
+          document.body.addEventListener('mousedown', setMouseStartPos);
+          document.body.addEventListener('mousemove', handleMouseMove);
+          document.body.addEventListener('mouseup', setMouseDownElPos);
+          document.body.addEventListener('mouseup', removeMousemoveEvent);
+          document.body.addEventListener('mouseleave', setMouseDownElPos);
+          document.body.addEventListener('mouseleave', removeMousemoveEvent);
+      } // 移除鼠标事件
+
+      function removeMousemoveEvent() {
+          document.body.removeEventListener('mousedown', setMouseStartPos);
+          document.body.removeEventListener('mousemove', handleMouseMove);
+          document.body.removeEventListener('mouseup', setMouseDownElPos);
+          document.body.removeEventListener('mouseup', removeMousemoveEvent);
+          document.body.removeEventListener('mouseleave', removeMousemoveEvent);
+          document.body.removeEventListener('mouseleave', setMouseDownElPos);
       }
-      const boundingClientRect = dialogEl.getBoundingClientRect()
-      dialogHeight = dialogEl.offsetHeight
-      dialogWidth = dialogEl.offsetWidth
-      dialogTop = boundingClientRect.top
-      dialogLeft = boundingClientRect.left
-      movedDialogTop = dialogTop
-      movedDialogLeft = dialogLeft
-      dialogEl.style.cssText += `position:fixed; top: ${dialogTop}px; left: ${dialogLeft}px; width:${dialogWidth}px; height:${dialogHeight}px;`
-      isInited = true
-    }
 
-    // 添加鼠标事件
-    function addMousemoveEvent (e: MouseEvent) {
-      e.preventDefault()
-      document.body.addEventListener('mousemove', handleMouseMove)
-      document.body.addEventListener('mouseup', removeMousemoveEvent)
-      document.body.addEventListener('mouseleave', removeMousemoveEvent)
-    }
+      function setMouseStartPos(e: MouseEvent) {
+          mouseStartX = e.x;
+          mouseStartY = e.y;
+      }
 
-    // 移除鼠标事件
-    function removeMousemoveEvent () {
-      document.body.removeEventListener('mousemove', handleMouseMove)
-      document.body.removeEventListener('mouseleave', removeMousemoveEvent)
-      document.body.removeEventListener('mouseup', removeMousemoveEvent)
-    }
+      function setMouseDownElPos() {
+          dialogX = newDialogX;
+          dialogY = newDialogY;
+      }
 
-    // 操作鼠标移动
-    function handleMouseMove (e: MouseEvent) {
-      const { movementX, movementY } = e
-      const outerWindowX = 200
-      const outerWindowY = 200
-      movedDialogTop = movedDialogTop + movementY
-      movedDialogLeft = movedDialogLeft + movementX
-      movedDialogTop = movedDialogTop <= -outerWindowY ? -outerWindowY : movedDialogTop
-      movedDialogTop = movedDialogTop >= window.innerHeight - dialogHeight + outerWindowY
-        ? window.innerHeight - dialogHeight + outerWindowY
-        : movedDialogTop
-      movedDialogLeft = movedDialogLeft <= -outerWindowX ? -outerWindowX : movedDialogLeft
-      movedDialogLeft = movedDialogLeft >= window.innerWidth - dialogWidth + outerWindowX
-        ? window.innerWidth - dialogWidth + outerWindowX
-        : movedDialogLeft
-      dialogEl.style.top = movedDialogTop + 'px'
-      dialogEl.style.left = movedDialogLeft + 'px'
-    }
+      function handleMouseMove(e: MouseEvent) {
+          const mouseDeltaX = e.x - mouseStartX;
+          const mouseDeltaY = e.y - mouseStartY;
+          newDialogX = dialogX + mouseDeltaX;
+          newDialogY = dialogY + mouseDeltaY;
+          //处理四周边界情况
+          if (newDialogY > window.innerHeight - dialogHeight) {
+              newDialogY = window.innerHeight - dialogHeight;
+          } else if (newDialogY < 0) {
+              newDialogY = 0;
+          }
+          if (newDialogX > window.innerWidth - dialogWidth) {
+              newDialogX = window.innerWidth - dialogWidth;
+          } else if (newDialogX < 0) {
+              newDialogX = 0;
+          }
+          dialogEl.style.left = newDialogX + 'px';
+          dialogEl.style.top = newDialogY + 'px';
+      }
   },
-}
-
-export default DragMove
+};
+export default DragMove;
+//# sourceMappingURL=index.js.map
