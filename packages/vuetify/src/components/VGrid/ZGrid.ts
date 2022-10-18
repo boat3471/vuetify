32f4ef567948bbdce7b38699cb829a615fa5afff
xinwenchao <boat3471@me.com>
2022-10-18 14:53:50 +0800

    ZGrid移除对dense的定制；

--- packages/vuetify/src/components/VGrid/ZGrid.ts
+++ packages/vuetify/src/components/VGrid/ZGrid.ts
@@ -18,14 +18,6 @@ export const ZCol = mixins(VCol).extend({
 
 export const ZRow = mixins(VRow).extend({
   name: 'z-row',
-  props: {
-    dense: {
-      type: Boolean,
-      default () {
-        return this.$themeStore.denseMode === true
-      },
-    },
-  },
 })
 
 export const ZSpacer = mixins(VSpacer).extend({
