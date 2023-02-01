54a786413f363c53adb18497f0d13ef1ac8cecd9
xinwenchao <boat3471@me.com>
2023-02-01 15:55:50 +0800

    测试createApp；优化透明色样式；

---
+++ packages/vuetify/dev/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="my-app">
+    <div>App-{{ msg }}</div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import { ZMessage } from '@zwd/z-ui'
+
+  export default {
+    components: {
+    },
+    data () {
+      return {
+        iconName: 'mdi-close',
+        msg: '',
+        time: Date.now(),
+        hide: false,
+        options: {
+          prependIcon: 'mdi-home',
+          prependInnerIcon: 'mdi-home',
+          appendIcon: 'mdi-home',
+          appendOuterIcon: 'mdi-home',
+        },
+        color: '',
+      }
+    },
+    watch: {
+      '$route.path' () {
+        if (this.$route) {
+          this.msg = JSON.stringify(this.$route.meta)
+        }
+      },
+    },
+    mounted () {
+      this.msg = JSON.stringify(this.$route.meta)
+    },
+    methods: {
+      click () {
+        ZMessage.error('asdasd', { duration: 0 })
+      },
+    },
+  }
+</script>
---
+++ packages/vuetify/dev/B.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>B</div>
+</template>
+
+<script>
+  export default {
+    name: 'B',
+    data () {
+      return {
+        title: 'B',
+      }
+    },
+  }
+</script>
